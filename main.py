9cca0da3967f71394ef55acdaf87783a49395d8c
MORIMOTO520212 <kanten520212@gmail.com>
2020-05-06 06:14:25 +0900

    1.6
    監視プログラム

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from time import sleep
 
+with open('setting.json', 'r') as f: # 設定ファイル読み込み
+    setting = json.load(f)
 
 
 #            初期設定            #
@@ -12,7 +14,7 @@ from time import sleep
 # chromeのプロファイルパス　※無い場合は空のファイルを作成
 PROFILEPATH = 'userdata'
 # メルカリデータパス　ここに監視する商品を登録する
-MERCARIDATAPATH = 'data/mercari_selling.json'
+MERCARIDATAPATH = setting['mercari_selling']
 
 
 
@@ -24,7 +26,7 @@ def FIND_CLASS_NAME(name):
     WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, name)))
     return driver.find_element_by_class_name(name).get_attribute('class')
 
-def FIND_CSS_SELECTOR(name):
+def FIND_TEXT_BY_CSS_SELECTOR(name):
     WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, name)))
     return driver.find_element_by_css_selector(name).text
 
@@ -33,10 +35,10 @@ def MONORATE(asin):
         print('ASINが正しく指定されていません。')
         return False
     driver.get('https://mnrate.com/item/aid/'+asin)
-    return FIND_CSS_SELECTOR('.price.used_price_color._btn_size_style.item_conditions_data_box')
+    return FIND_TEXT_BY_CSS_SELECTOR('.price.used_price_color._btn_size_style.item_conditions_data_box')
 
 def BOT(data):
-    driver.get(data)
+    driver.get("https://www.mercari.com/jp/items/"+data)
     mer_slt_status   = FIND_CLASS_NAME('item-buy-btn')
     mer_slt_price    = FIND_CLASS_NAME('item-price')
     mer_slt_price    = mer_slt_price.replace('¥','').replace(',','')
@@ -48,16 +50,13 @@ def BOT(data):
     mnrate_low_price = MONORATE(mer_slt_d['asin'])
     mnrate_low_price = mnrate_low_price.replace('￥','').replace(',','')
 
-    if mer_slt_price > mnrate_low_price:
-        # 金額がモノレート最低金額よりも高くなった場合
+    if mer_slt_price > mnrate_low_price: # 金額がモノレート最低金額よりも高くなった場合
         return 'than{}'.format(int(mer_slt_price)-int(mnrate_low_price))
     
-    if mer_slt_price < mnrate_low_price:
-        # 金額がモノレート最低金額よりも低くなった場合
+    if mer_slt_price < mnrate_low_price: # 金額がモノレート最低金額よりも低くなった場合
         return 'down{}'.format(int(mnrate_low_price)-int(mer_slt_price))
 
-    if mer_slt_price == mnrate_low_price:
-        # 金額が同じ場合
+    if mer_slt_price == mnrate_low_price: # 金額が同じ場合
         return 0
 
 
@@ -66,17 +65,25 @@ if __name__=='__main__':
         mer_sel_d = json.load(f)
 
     for mer_slt_d in mer_sel_d:
+        mercariId = mer_slt_d['mercariId']
 
-        bot_result = BOT(mer_slt_d['href'])
-        if bot_result == 'soldOut':
-            print('売り切れ')
-        elif 'than' in bot_result:
-            than_amount = bot_result.replace('than','')
-            print('モノレートよりも{}円高い'.format(than_amount))
-        elif 'down' in bot_result:
-            down_amount = bot_result.replace('down','')
-            print('モノレートよりも{}円低い')
-        elif not bot_result:
-            print('モノレートと同じ金額')
+        if mer_slt_d['status']!='soldout':
+            bot_result = BOT(mercariId) # スクレイピングで商品の状態を確認する
+
+            if bot_result == 'soldOut':
+                print('MercariID:{} 売り切れ'.format(mercariId))
+
+            elif 'than' in bot_result:
+                than_amount = bot_result.replace('than','')
+                print('MercariID:{} モノレートよりも{}円高い'.format(mercariId, than_amount))
+
+            elif 'down' in bot_result:
+                down_amount = bot_result.replace('down','')
+                print('MercariID:{} モノレートよりも{}円低い'.format(mercariId, down_amount))
+
+            elif not bot_result:
+                print('MercariID:{} モノレートと同じ金額'.format(mercariId))
+        else:
+            print('MercariID:{} 売り切れ'.format(mercariId))
 
     driver.close()
